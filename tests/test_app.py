d4b18f16bbba28ea0dd373551ce52175143a8a0c
Raul Rory <raulrory@gmail.com>
2024-10-13 23:46:08 -0300

    test: create file conftest and format file test_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,31 @@
 from http import HTTPStatus
-from fast_zero.app import app
-from fastapi.testclient import TestClient
 
 
-def test_read_root_return_ok():
-    client = TestClient(app)
-
+def test_read_root_return_ok(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
 
-def test_read_root_html_return_ok():
-    client = TestClient(app)
-
+def test_read_root_html_return_ok(client):
     response = client.get('/challange')
     response_html = '<h1> Olá Mundo </h1>'
 
     assert response.status_code == HTTPStatus.OK
     assert response_html in response.text
+
+def test_create_user(client):
+    response = client.post("/users", 
+                json={
+                    'username': 'user_test',
+                    'password': 'password',
+                    'email': 'test@io.com.br'
+                }
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'user_test',
+        'email': 'test@io.com.br',
+        'id': 1
+    }
