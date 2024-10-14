b418e3599e79641b432a1b0f5b333307e53768b9
Raul Rory <raulrory@gmail.com>
2024-10-13 22:54:14 -0300

    test: function read_root_html return 200

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,12 @@ def test_read_root_return_ok():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+def test_read_root_html_return_ok():
+    client = TestClient(app)
+
+    response = client.get('/challange')
+    response_html = '<h1> Olá Mundo </h1>'
+
+    assert response.status_code == HTTPStatus.OK
+    assert response_html in response.text
