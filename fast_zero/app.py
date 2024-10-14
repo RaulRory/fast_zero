6ae527bdd1606f76fe00fdb6a80615a9a06d5dcb
Raul Rory <raulrory@gmail.com>
2024-10-13 23:24:24 -0300

    feat: create news schemas for user and function create_user

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from http import HTTPStatus
-from fast_zero.schemas import Message
+from fast_zero.schemas import (
+    Message, 
+    UserSchema, 
+    UserPublic,
+    UserDB
+  )
 
-app = FastAPI()  
+app = FastAPI()
+database = []
+
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)  
 def read_root():  
@@ -20,3 +27,14 @@ def read_root_html():
         <h1> Olá Mundo </h1>
       </body>
     </html>"""
+
+@app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_users(user: UserSchema):
+    user_with_id = UserDB(
+        id=len(database) + 1, 
+        **user.model_dump()
+    )
+
+    database.append(user_with_id)
+
+    return user_with_id
