24f30541c85fc92a722008a24f5de3c9b97f6075
Raul Rory <raulrory@gmail.com>
2024-10-13 22:53:03 -0300

    feat: create function read_root_html and class Message

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from http import HTTPStatus
+from fast_zero.schemas import Message
 
 app = FastAPI()  
 
-@app.get('/')  
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)  
 def read_root():  
     return {'message': 'Olá Mundo!'}
+
+@app.get('/challange', status_code=HTTPStatus.OK, response_class=HTMLResponse)  
+def read_root_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
