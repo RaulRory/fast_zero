e9f5c631631835f040c384410eac743cde12ce69
Raul Rory <raulrory@gmail.com>
2024-10-14 00:43:31 -0300

    feat: create new endpoints, put, delete and get with id

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from http import HTTPStatus
 from fast_zero.schemas import (
     Message, 
     UserDB,
+    UserList,
     UserPublic,
     UserSchema, 
   )
@@ -29,7 +30,7 @@ def read_root_html():
     </html>"""
 
 @app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_users(user: UserSchema):
+def create_user(user: UserSchema):
     user_with_id = UserDB(
         id=len(database) + 1, 
         **user.model_dump()
@@ -38,3 +39,46 @@ def create_users(user: UserSchema):
     database.append(user_with_id)
 
     return user_with_id
+
+@app.get('/users', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+@app.get('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def find_user(user_id: int):
+  if user_id < 1 or user_id > len(database) + 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+  user = database[user_id - 1]
+  return user
+
+
+@app.put('/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    
+    if user_id < 1 or user_id > len(database) + 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+  
+    user_with_id = UserDB(
+        id=user_id, 
+        **user.model_dump()
+    )
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(database) + 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    
+    del database[user_id - 1]
+
+    return {'message': 'User deletd!'}
